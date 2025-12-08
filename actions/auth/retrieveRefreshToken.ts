0715a2d74d5e67e997c7ca79945188db3b6a0145
mahmudul <mahmudul35-218@diu.edu.bd>
2025-12-08 21:45:27 +0600

    dgdgdg

--- actions/auth/retrieveRefreshToken.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-import { cookies } from 'next/headers';
-
-export default async function retrieveRefreshToken() {
-  const cookieStore = await cookies();
-  return cookieStore.get('refresh_token')?.value;
-}
