6b74bcad2f24facee16fbe4b0fd0517c5a24b0e8
mahmudul <mahmudul35-218@diu.edu.bd>
2025-12-08 21:44:59 +0600

    dgdgdg

--- actions/auth/storeRefreshToken.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-import { cookies } from 'next/headers';
-
-export default async function storeRefreshToken(token: string) {
-  const cookieStore = await cookies();
-  return cookieStore.set('refresh_token', token);
-}
