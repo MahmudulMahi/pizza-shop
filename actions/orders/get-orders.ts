c083d8572e7594b1ad7806cda0ad407e4bf4c392
mahmudul <mahmudul35-218@diu.edu.bd>
2025-12-16 22:14:55 +0600

    order create

--- actions/orders/get-orders.ts
+++ actions/orders/get-orders.ts
@@ -4,13 +4,7 @@ import { fetchApiClient } from '@/lib/oneentry';
 import { cookies } from 'next/headers';
 
 export const getOrders = async () => {
-  try {
-    const apiClient = await fetchApiClient();
-    const accessToken = (await cookies()).get('access_token')?.value;
 
-    if (!accessToken) {
-      throw new Error('Missing access token.');
-    }
 
     const orders = await apiClient?.Orders.setAccessToken(
       accessToken
