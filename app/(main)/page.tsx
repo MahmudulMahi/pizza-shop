db43fd55ae735c00bd0cd977b14346c37688eee1
mahmudul <mahmudul35-218@diu.edu.bd>
2026-01-05 22:01:24 +0600

    fgfgfg

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -448,15 +448,7 @@ export default function HomePage() {
           </div>
         )}
 
-        {products.map((catalog) => (
-          <ProductCatalog
-            key={catalog?.id}
-            title={catalog?.localizeInfos?.title as string}
-            products={
-              catalog.catalogProducts.items as unknown as IProductsEntity[]
-            }
-          />
-        ))}
+
       </main>
     </div>
   );
