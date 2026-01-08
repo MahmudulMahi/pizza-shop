7b42e91f5967dfc74aaaa679f2a63ee9da1d01d2
mahmudul <mahmudul35-218@diu.edu.bd>
2026-01-08 21:13:36 +0600

    fetch product details

--- actions/catalog/getProductDetails.ts
+++ actions/catalog/getProductDetails.ts
@@ -4,7 +4,9 @@ import { fetchApiClient } from '@/lib/oneentry';
 export const getProductDetails = async (productId: number) => {
   const apiClient = await fetchApiClient();
 
-
+  if (!productId) {
+    throw new Error('Product ID is required.');
+  }
 
   try {
     const product = await apiClient?.Products.getProductById(
